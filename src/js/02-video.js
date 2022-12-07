4c8f60f1b8d11e3b4512ab58ba18742a71215d02
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-07 17:35:18 +0100

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Vimeo from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+//saving current playtime
+player.on('timeupdate', throttle(evt => {
+   localStorage.setItem("videoplayer-current-time", evt.seconds)
+}, 1000));
+
+//loading current playtime after reload
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
