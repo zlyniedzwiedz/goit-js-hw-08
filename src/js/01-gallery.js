703d631cc030b80689b5602dc5c41a3cfb148fca
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-07 16:58:17 +0100

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+    <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+  </a>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData: "alt", captionDelay: 250 });
