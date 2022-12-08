5911e99200a1daa4a69e25428c8c8d60695ede23
Michal Raszczyk <mraszczyk@gmail.com>
2022-12-08 20:11:12 +0100

    feedback fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,49 @@
 var throttle = require('lodash.throttle');
 
-// const email = document.querySelector("email");
-// const message = document.querySelector("message");
+const email = document.querySelector('email');
+const message = document.querySelector('message');
 const formInput = document.querySelector('.feedback-form');
-const submitBtn = document.querySelector('button[type=submit]')
-
-const storageUpdate = evt => {
- 
-    const completeForm = {
-    email: formInput.email.value,
-    message: formInput.message.value,
-  };
-
-  localStorage.setItem('feedback-form-state', JSON.stringify(completeForm));
-};
-formInput.addEventListener('input', throttle(storageUpdate, 500));
+const submitBtn = document.querySelector('button[type=submit]');
 
 const stateCheck = evt => {
   const savedInput = localStorage.getItem('feedback-form-state');
   const parsedInput = JSON.parse(savedInput);
   if (savedInput === null) {
     return;
+  }
+  formInput.email.value = parsedInput.email;
+  formInput.message.value = parsedInput.message;
+  if (formInput.email.value === '' && formInput.message.value === '') {
+    submitBtn.disabled = true;
   } 
-    formInput.email.value = parsedInput.email;
-    formInput.message.value = parsedInput.message;
-  
 };
+
+submitBtn.disabled = true;
+
+const storageUpdate = evt => {
+  const completeForm = {
+    email: formInput.email.value,
+    message: formInput.message.value,
+  };
+  if (completeForm.email != '' && completeForm.message != '') {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(completeForm));
+};
+
+formInput.addEventListener('input', throttle(storageUpdate, 500));
+
 window.addEventListener('Load', stateCheck());
 
-formInput.addEventListener("submit", (evt) => {
-    evt.preventDefault()
-    console.log(localStorage.getItem("feedback-form-state"))
-    localStorage.removeItem("feedback-form-state")
-    formInput.email.value = ""
-    formInput.message.value = ""
-})
-
+formInput.addEventListener('submit', evt => {
+  evt.preventDefault();
+  submitBtn.disabled = true;
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  formInput.email.value = '';
+  formInput.message.value = '';
+  
+});
